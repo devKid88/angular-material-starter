1f1829bea261d08a2d345b5af8d33b481c36612a
Nemanja Marinkovic <nemanjamarinkovic88@gmail.com>
2018-08-02 14:11:56 +0200

    Added core and shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoreModule
   ],
   imports: [
     BrowserModule,
