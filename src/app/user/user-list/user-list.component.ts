83ddebf06759fe03ec4989c9cc29f2edac129034
Nemanja Marinkovic <nemanjamarinkovic88@gmail.com>
2018-09-10 17:43:32 +0200

    Added table for listing users with pagination and filter

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 
 import { UserService } from '../core/user.service';
 import { UserModel } from '../core/user.model';
+
 
 @Component({
   selector: 'app-user-list',
@@ -13,6 +15,13 @@ export class UserListComponent implements OnInit {
   users: UserModel[];
   totalUsers: number;
 
+  // DATA TABLE CONFIG
+  displayedColumns: string[] = ['id', 'name'];
+  dataSource: MatTableDataSource<UserModel>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  // END DATA TABLE CONFIG
+
   constructor(
     private userService: UserService
   ) { }
@@ -21,10 +30,25 @@ export class UserListComponent implements OnInit {
     this.loadUsers();
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
   private loadUsers() {
     this.userService.getAll().subscribe(response => {
-        this.users = response.items;
-        this.totalUsers = response.total_count;
-      });
+      this.users = response.items;
+      this.totalUsers = response.total_count;
+      this.setDataSource(this.users);
+    });
+  }
+
+  private setDataSource(users: UserModel[]) {
+    this.dataSource = new MatTableDataSource(users);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,11 +4,18 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserService } from './core/user.service';
+import { MatFormFieldModule, MatTableModule, MatInputModule, MatPaginatorModule } from '@angular/material';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    MaterialModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule
   ],
   declarations: [UserListComponent],
   providers: [
